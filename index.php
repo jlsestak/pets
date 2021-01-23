b2318203dd83871ba9f89cd4aea14ba66b3edc81
Jessica Sestak <jsestak2@mail.greenriver.edu>
2021-01-22 22:25:59 -0500

    fixing errors with config

--- index.php
+++ index.php
@@ -11,6 +11,10 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 $f3->set('DEBUG', 3);
 
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
 
 
 //Run fat free
